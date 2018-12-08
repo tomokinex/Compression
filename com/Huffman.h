49bcf9aaaa87da3539af30d473568f198e4e4840
tomokin <kurobist@gmail.com>
2018-12-09 00:18:44 +0900

    huffman木作成まで

--- com/Huffman.h
+++ com/Huffman.h
@@ -6,6 +6,7 @@ namespace Huff{
         public:
         int m_num;
         unsigned char m_val;
+        //右を小さくする
         tree* m_parent;
         tree* m_left;
         tree* m_right;
@@ -22,8 +23,23 @@ namespace Huff{
         bool Isparent(){
             return m_parent != nullptr;
         }
+        bool Ischild(){
+            return (m_left != nullptr) || (m_right != nullptr);
+        }
         int Isnum(){
             return m_num;
+        }
+        void Setparent(tree* parent){
+            m_parent = parent;
+        }
+        void Setright(tree* right){
+            m_right = right;
+        }
+        void Setleft(tree* left){
+            m_left = left;
+        }
+        void Setnum(int num){
+            m_num = num;
         }
     };
 
@@ -33,7 +49,7 @@ namespace Huff{
         int index1 = -1;
         int index2 = -1;
         for(int i=0;i<hist.size();i++){
-            if(!hist[i].Isparent()){
+            if(!hist[i].Isparent() && hist[i].Isnum() > 0){
                 if(min1 > hist[i].Isnum()){
                     min2 = min1;
                     index2 = index1;
@@ -53,10 +69,29 @@ namespace Huff{
 
 
     void Huff_compress(std::vector<unsigned char>& file, std::vector<unsigned char>& comp){
-        std::vector<tree> m_tree(257);
+        std::vector<tree> m_tree(256,tree());
+        //histgram作成
         for(int i=0;i<file.size();i++){
             m_tree[(int)file[i]].add();
         }
+        //treeの作成
+        int min_index1 = -2;
+        int min_index2 = -2;
+        while(min_index1 != -1 && min_index2 != -1){
+            min_search(m_tree, &min_index1, &min_index2);
+            if(min_index1 != -1 && min_index2 != -1){
+                tree temp_tree = tree();
+                temp_tree.Setright(&m_tree[min_index1]);
+                temp_tree.Setleft(&m_tree[min_index2]);
+                temp_tree.Setnum(m_tree[min_index1].Isnum()+m_tree[min_index2].Isnum());
+                m_tree.push_back(temp_tree);
+                m_tree[min_index1].Setparent(&m_tree[m_tree.size()-1]);
+                m_tree[min_index2].Setparent(&m_tree[m_tree.size()-1]);
+            }else{
+                break;
+            }
+        }
+
     }
     void Huff_decompress(std::vector<unsigned char>& in, std::vector<unsigned char>& decomp){
     }
